47a95117819eb86405e97fc7cd68ddb6a36f01b3
Ava Taylor <127423117+AvaTaylor258@users.noreply.github.com>
2024-04-09 15:38:36 +0000

    added sidenav!

---
+++ script.js
@@ -0,0 +1,11 @@
+function openNav() {
+	document.getElementById("sidenav").style.width = "200px";
+	document.getElementById("sidenav").style.paddingLeft = "10px";
+	document.getElementById("sidenav").style.paddingRight = "10px";
+}
+
+function closeNav() {
+	document.getElementById("sidenav").style.width = "0px";
+	document.getElementById("sidenav").style.paddingLeft = "0px";
+	document.getElementById("sidenav").style.paddingRight = "0px";
+}
